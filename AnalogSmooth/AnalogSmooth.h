49a89c3231d3fe0e5419897231e1d9fd109a9cb3
Michael Thessel <michael.thessel@gmx.de>
2012-12-21 19:43:37 -0800

    Initial commit

---
+++ AnalogSmooth/AnalogSmooth.h
@@ -0,0 +1,26 @@
+/*
+  AnalogSmooth.h - Library to smooth analog signal 
+  jitter by averaging concurrent readings.
+  Created by Michael Thessel.
+*/
+ 
+#ifndef AnalogSmooth_h
+#define AnalogSmooth_h
+
+#include "Arduino.h"
+
+class AnalogSmooth
+{
+	public:
+		AnalogSmooth(unsigned int windowSize);
+		AnalogSmooth();
+		float analogReadSmooth(uint8_t pin);
+	private:
+		unsigned int _windowSize;
+		uint8_t _pin;
+		float _analog[100];
+		unsigned int _analogPointer;
+		unsigned int _maxPointer;
+		void _init(unsigned int windowSize);
+};
+#endif
---
+++ AnalogSmooth/examples/analogSmooth/analogSmooth.ino
@@ -0,0 +1,42 @@
+/*
+  Smoothens signal jitter on analog inputs by averaging 
+  concurrent readings. The number of readings averaged can 
+  range from 1 (no smooting) to 100.
+  
+  The more readings you average the more consistent the signal 
+  becomes. At the same time it takes longer to detect actual 
+  changes in the signal.
+*/
+#include <AnalogSmooth.h>
+
+int analogPin = 1;
+
+// Defaults to window size 10
+AnalogSmooth as = AnalogSmooth();
+
+// Window size can range from 1 - 100
+AnalogSmooth as100 = AnalogSmooth(100);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Regular reading
+  float analog = analogRead(analogPin);
+  Serial.print("Non-Smooth: ");
+  Serial.println(analog);
+  
+  // Smoothing with window size 10
+  float analogSmooth = as.analogReadSmooth(analogPin);
+  Serial.print("Smooth (10): ");  
+  Serial.println(analogSmooth);
+
+  // Smoothing with window size 100
+  float analogSmooth100 = as100.analogReadSmooth(analogPin);
+  Serial.print("Smooth (100): ");  
+  Serial.println(analogSmooth100);
+
+  Serial.println("");
+  delay(1000);
+}
